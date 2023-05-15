87f555e47909ad3f597fc12d13971e7898af7108
ojt-aspher <133208022+ojt-aspher@users.noreply.github.com>
2023-05-15 09:32:59 +0800

    files upload

---
+++ web-client/src/pages/designation/Index.vue
@@ -0,0 +1,28 @@
+<template>
+  <section>
+    <v-card color="transparent" flat>
+      <v-card-title class="display-1 font-weight-bold"
+        >Manage Designations</v-card-title
+      >
+      <v-card-subtitle
+        >View, add, edit, and delete employee designation here.</v-card-subtitle
+      >
+      <v-card-text>
+        <custom-breadcrumbs :routes="breadcrumbRoutes"></custom-breadcrumbs>
+      </v-card-text>
+      <router-view></router-view>
+    </v-card>
+  </section>
+</template>
+<script>
+import CustomBreadcrumbs from "../../components/custom/Breadcrumbs";
+export default {
+  components: { CustomBreadcrumbs },
+  computed: {
+    breadcrumbRoutes() {
+      const routes = this.$route.meta.breadcrumbs;
+      return routes ? routes : [];
+    },
+  },
+};
+</script>
